d066aa9e1ed0f7696d419a171e3d7e239a3bc713
krishnanx <abhishekkrishnan2006@gmail.com>
2025-03-25 23:54:31 +0530

    CORS change

--- express.js
+++ express.js
@@ -7,7 +7,13 @@ import http from "http";
 import { supabase } from "./Supabase.js/supabase.js";
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
-app.use(cors());
+app.use(
+    cors({
+        origin: "http://nutrigen.myprojects.studio", // Allow only this domain
+        methods: ["GET", "POST"], // Allow specific methods
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
